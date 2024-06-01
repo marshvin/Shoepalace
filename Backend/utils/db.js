c0001fd4167ffc924738d086b1ce332f53ff0ab8
marshvin <vincentmarsh01@gmail.com>
2024-06-01 22:35:12 +0300

    Postgresql and API deployment

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -8,6 +7,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
