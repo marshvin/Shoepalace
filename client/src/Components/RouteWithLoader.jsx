a598c7dee392f93260e0726c9c1fd345669936ee
marshvin <vincentmarsh01@gmail.com>
2024-08-20 17:41:53 +0300

    Add to cart improvements

--- client/src/Components/RouteWithLoader.jsx
+++ client/src/Components/RouteWithLoader.jsx
@@ -1,5 +1,3 @@
-// src/components/RouteWithLoader.jsx
-
 import React, { useState, useEffect } from 'react';
 import Spinner from './Spinner';
 
@@ -7,9 +5,25 @@ const RouteWithLoader = ({ component: Component, ...rest }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-   
-    const timer = setTimeout(() => setLoading(false), 3000);
-    return () => clearTimeout(timer);
+    // Event listener for when the page content is fully loaded
+    const handleLoad = () => {
+      setLoading(false);
+    };
+
+    // Event listener for when the DOM is fully loaded and parsed
+    const handleDOMContentLoaded = () => {
+      // You might want to load additional resources or set up logic here
+    };
+
+    // Attach the event listeners
+    window.addEventListener('load', handleLoad);
+    document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
+
+    // Clean up event listeners on component unmount
+    return () => {
+      window.removeEventListener('load', handleLoad);
+      document.removeEventListener('DOMContentLoaded', handleDOMContentLoaded);
+    };
   }, []);
 
   return loading ? <Spinner /> : <Component {...rest} />;
