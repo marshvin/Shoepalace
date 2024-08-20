6f4a4ae6ab4bacd26d2d86b275b88c8853787be5
marshvin <vincentmarsh01@gmail.com>
2024-08-20 17:47:37 +0300

    Add to cart improvements

--- client/src/Components/RouteWithLoader.jsx
+++ client/src/Components/RouteWithLoader.jsx
@@ -5,24 +5,14 @@ const RouteWithLoader = ({ component: Component, ...rest }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Event listener for when the page content is fully loaded
     const handleLoad = () => {
       setLoading(false);
     };
 
-    // Event listener for when the DOM is fully loaded and parsed
-    const handleDOMContentLoaded = () => {
-      // You might want to load additional resources or set up logic here
-    };
-
-    // Attach the event listeners
     window.addEventListener('load', handleLoad);
-    document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
 
-    // Clean up event listeners on component unmount
     return () => {
       window.removeEventListener('load', handleLoad);
-      document.removeEventListener('DOMContentLoaded', handleDOMContentLoaded);
     };
   }, []);
 
